a0d86708c88050deeee31f48e79115b38d5ebb56
Christian Segundo <christian.segundo@cabify.com>
2022-12-06 09:57:36 +0100

    remove comments

--- day_06.zig
+++ day_06.zig
@@ -50,29 +50,3 @@ pub fn puzzle_2(input: []const u8) Result {
 
     return .{ .int = @intCast(i32, i) };
 }
-
-//pub fn puzzle_2(input: []const u8) Result {
-//var iter = std.mem.split(u8, input, "\n");
-
-//var count: u16 = 0;
-//mainLoop: while (true) {
-//var parts = std.mem.zeroes([3]u64);
-
-//for (parts) |*p| {
-//const line = iter.next() orelse break :mainLoop;
-//for (line) |_, i| {
-//p.* |= @as(u64, 1) << @intCast(u6, line[i] - 65);
-//}
-//}
-
-//count += char_to_priority(@intCast(u8, 65 + 63) - @clz(parts[0] & parts[1] & parts[2]));
-//}
-
-//return .{ .int = count };
-//}
-
-//fn char_to_priority(char: u8) u8 {
-//if (char >= 65 and char <= 90)
-//return char - 38;
-//return char - 96;
-//}
